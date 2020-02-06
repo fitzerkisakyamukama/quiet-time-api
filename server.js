dc0d166ae812ce685e6da81d8bbf63a67e8056ed
Michael <kabalimichael03@gmail.com>
2020-02-06 18:14:47 +0300

    add pocfile env

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('/', function(req, res) {
 });
 
 // Create a Server
-var server = app.listen(8080, function() {
+var server = app.listen(process.env.PORT || 8080, function() {
   var host = server.address().address
   var port = server.address().port
 
